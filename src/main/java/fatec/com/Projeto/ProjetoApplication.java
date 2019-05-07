6bab910844ed4d81608be8c76ecfd1022a298a68
lukasribeiro <luckas.ribeiro2015@gmail.com>
2019-05-07 10:21:07 -0300

    projeto inicial

---
+++ src/main/java/fatec/com/Projeto/ProjetoApplication.java
@@ -0,0 +1,13 @@
+package fatec.com.Projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApplication.class, args);
+	}
+
+}
